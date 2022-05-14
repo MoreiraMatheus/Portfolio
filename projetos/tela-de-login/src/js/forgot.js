f9943fa261c3ac2553554bac14193803b3b337d5
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-05-14 18:41:33 -0300

    iniciando scripts

---
+++ projetos/tela-de-login/src/js/forgot.js
@@ -0,0 +1,7 @@
+function enviar_codigo(){
+    var hiden = document.getElementById('escondido')
+    var msg = document.getElementById('p')
+
+    msg.style.display = 'block'
+    hiden.style.marginBottom = '170px'
+}
