6fe5a1c5fef0798d0c64f0155c30ad8ed34e4ba1
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-05-15 14:42:51 -0300

    brincando com JS

--- projetos/tela-de-login/src/js/index.js
+++ projetos/tela-de-login/src/js/index.js
@@ -9,14 +9,36 @@ function ver_senha(){
 }
 
 function entrar(){
-    window.alert('botão ok')
     var login = document.getElementById('login')
+    var senha = document.getElementById('senha')
     var login_user = login.value
-    window.alert(login_user)
-    // if(login_user == 'admin' && senha_user == 'admin'){
-    //     window.alert('bem vindo')
-    // }
-    // else{
-    //     window.alert('usuário não indentificado')
-    // }
+    var senha_user = senha.value
+
+    if(login_user.length == 0 || senha_user.length == 0){
+        if(login_user.length == 0){
+            window.alert('digite seu login')
+        }
+        if(senha_user.length == 0){
+            window.alert('digite uma senha')
+        }
+    }
+    else{
+        if(login_user == 'admin' && senha_user == 'admin'){
+            location.href = 'bem-vindo.html'
+        }
+        else{
+            window.alert('login ou senha incorretos')
+        }
+    }
+}
+
+function nome_user(){
+    // encontrar uma maneira de dizer o nome do usuário ao carregar página
+    var local = location.href 
+    if(document.URL == 'bem-vindo.html'){
+        window.onload = window.alert('estou funcionando')
+    }
+    // var bv = document.getElementById('bem-vindo')
+    // bv.innerHTML += 'user'
+    // login_user
 }
