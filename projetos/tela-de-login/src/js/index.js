c0fdb035b59aee2029c669020302ff9a9ee686fe
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-05-14 21:53:14 -0300

    iniciando validação simples

--- projetos/tela-de-login/src/js/index.js
+++ projetos/tela-de-login/src/js/index.js
@@ -1,9 +1,22 @@
 function ver_senha(){
-    var senha = document.getElementById('senha')
+    let senha = document.getElementById('senha')
     if(senha.type == 'password'){
         senha.type = "Text"
     }
     else{
         senha.type = "password"
     }
+}
+
+function entrar(){
+    window.alert('botão ok')
+    var login = document.getElementById('login')
+    var login_user = login.value
+    window.alert(login_user)
+    // if(login_user == 'admin' && senha_user == 'admin'){
+    //     window.alert('bem vindo')
+    // }
+    // else{
+    //     window.alert('usuário não indentificado')
+    // }
 }
