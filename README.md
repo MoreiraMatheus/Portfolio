6ccd45c6683b2826baf85e8d60fae72c8eb272ab
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-05-21 06:37:55 -0300

    atualizando README

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Portfolio
- 
+
+## Quem eu sou
+olá, me chamo Matheus e esse é meu portifólio, será aqui que eu irei compartilhar meus estudos e experiências na área da programação
+
+## O que faço
+Atualmente sou estudante de Análise e desenvolvimento de sistemas e busco sempre aprimorar meus conhecimentos
+
+## O que já fiz
+Confira abaixo alguns dos meus projetos:
+
+* Tela de login
+* Calculadora 
+* Pokedex
--- projetos/tela-de-login/src/js/forgot.js
+++ projetos/tela-de-login/src/js/forgot.js
@@ -1,7 +1,12 @@
 function enviar_codigo(){
     var hiden = document.getElementById('escondido')
     var msg = document.getElementById('p')
-
-    msg.style.display = 'block'
-    hiden.style.marginBottom = '170px'
+    var email = document.getElementById('email')
+    if(email.value.length == 0){
+        window.alert('Digite seu e-mail')
+    }
+    else{
+        msg.style.display = 'block'
+        hiden.style.marginBottom = '170px'
+    }
 }
