f51e81c007d2b7cd4e6492f2c8f465d9bff8c1f9
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-06-19 14:33:43 -0300

    animações adicionadas

--- portfolio/src/js/nav.js
+++ portfolio/src/js/nav.js
@@ -3,3 +3,13 @@ function rolar(caixa){
     const distanciatop = box.offsetTop
     window.scroll(0, distanciatop - 35)
 }
+
+function tema(){
+    const checkbox = document.getElementById("tema-checkbox");
+    if(checkbox.checked){
+        console.log('tema escuro')
+    }
+    else{
+        console.log('tema claro')
+    }
+};
